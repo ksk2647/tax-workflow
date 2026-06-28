fa5bfda9b8247c0dba39745a55a6ba1c3b47bb37
Sunkyung Kim <ksk3844@gmail.com>
2026-06-28 10:03:06 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/skkim/taxworkflow/TaxWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.skkim.taxworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxWorkflowApplication.class, args);
+	}
+
+}
